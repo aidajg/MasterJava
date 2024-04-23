43e5226a097d89e940675b2435db2490cd640826
aidajg <151662136+aidajg@users.noreply.github.com>
2024-04-23 15:19:12 +0200

    Delete biblioteca directory

--- biblioteca/Revistas.java
+++
@@ -1,8 +0,0 @@
-package ejercicios.biblioteca;
-
-public class Revistas extends Documento{
-	//constructor padre
-	public Revistas(String codigo, String titulo, boolean prestado) {
-		super(codigo, titulo, prestado);
-	}
-}
